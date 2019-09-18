b5889d154a3f74b3d4b3b0a6d548ddd9a4aad1ea
zgfrank <zgfzmj@outlook.com>
2019-09-18 16:53:16 +0800

    我令狐冲修改的剑法天下第一

--- huashanjianfa.java
+++ huashanjianfa.java
@@ -1,5 +1,6 @@
 public class HuaShanJianFa{
 	public HuaShanJianFa(){
-		Syste,.out.println("华山剑法，天下第一");
+		System.out.println("我令狐冲才是天下第一");
+		System.out.println("华山剑法，天下第一"); //修改System错误
 	}
 }
