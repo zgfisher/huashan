35b464f6f5af053ebdd3fd83ae8334f156db99eb
zgfrank <zgfzmj@outlook.com>
2019-09-18 21:09:14 +0800

    我要制造冲突

--- huashanjianfa.java
+++ huashanjianfa.java
@@ -2,6 +2,7 @@ public class HuaShanJianFa{
 	public HuaShanJianFa(){
 		System.out.println("我令狐冲才是天下第一");
 		System.out.println("我会独孤九剑！");
+		System.out.println("我会辟邪剑法!"); // 岳不群说
 		System.out.println("华山剑法，天下第一"); //修改System错误
 	}
 }
