88991f961e494c54409eb167c28c9fbe70a0d842
zgfrank <zgfzmj@outlook.com>
2019-09-19 13:37:21 +0800

    东方不败添加葵花宝典

--- huashanjianfa.java
+++ huashanjianfa.java
@@ -4,5 +4,6 @@ public class HuaShanJianFa{
 		System.out.println("我会独孤九剑,我要雄霸天下"); //令狐冲增加了一句
 		System.out.println("我会辟邪剑法!"); // 岳不群说
 		System.out.println("华山剑法，天下第一"); //修改System错误
+		System.out.println("葵花宝典，独霸武林");
 	}
 }
